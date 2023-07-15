30aea2eb31b87d7b8c7258f5f2b92e305914d866
league <liangyuanli2020@gmail.com>
2023-07-15 11:31:14 -0700

    work

--- src/_03_More_Algorithms/Algorithms.java
+++ src/_03_More_Algorithms/Algorithms.java
@@ -1,5 +1,7 @@
 package _03_More_Algorithms;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /*
@@ -10,52 +12,140 @@ public class Algorithms {
 	public static String multiply(int num1, int num2) {
 		int answer = num1 * num2;
 		String stringAnswer = num1 + " x " + num2 + " = " + answer;
-		
+
 		return stringAnswer;
 	}
 
 	public static boolean isPrime(int num) {
-		for(int i = 2; i < (num/2)+1; i++) {
-			if(num%i == 0) {
+		for (int i = 2; i < (num / 2) + 1; i++) {
+			if (num % i == 0) {
 				return false;
 			}
 		}
-	
+
 		return true;
 
 	}
 
 	public static boolean isSquare(int num) {
-	
-	return false;
-	}
+		double number = num;
+		double root = Math.sqrt(number);
+		int intRoot = (int) Math.sqrt(number);
+		if (root == intRoot) {
+			return true;
+		}
 
-	public static boolean isCube(int num) {
 		return false;
 	}
 
+	public static boolean isCube(int num) {
+		double number = num;
+		double cbRoot = Math.cbrt(number);
+		int intRoot = (int) Math.cbrt(number);
+		if (intRoot == cbRoot) {
+			return true;
+		}
+
+		return false;
+	}
+
+	// EEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+
 	public static int findBrokenEgg(List<String> eggs) {
+		for (int i = 0; i < eggs.size(); i++) {
+			if (eggs.get(i).equalsIgnoreCase("cracked")) {
+				return i;
+			}
+		}
+
 		return 0;
 	}
 
 	public static int countPearls(List<Boolean> oysters) {
-		return 0;
+		int pearlCount = 0;
+		for (int i = 0; i < oysters.size(); i++) {
+			if (oysters.get(i) == true) {
+				pearlCount++;
+			}
+		}
+
+		return pearlCount;
 	}
 
 	public static double findTallest(List<Double> peeps) {
-		return 0.0;
+		double tallestPerson = 0.0;
+		for (int i = 0; i < peeps.size(); i++) {
+			if (peeps.get(i) > tallestPerson) {
+				tallestPerson = peeps.get(i);
+			}
+
+		}
+		return tallestPerson;
 	}
 
 	public static String findLongestWord(List<String> words) {
-		return null;
+		String longestString = "";
+		for (int i = 0; i < words.size(); i++) {
+			if (words.get(i).length() > longestString.length()) {
+				longestString = words.get(i);
+			}
+		}
+
+		return longestString;
 	}
 
 	public static boolean containsSOS(List<String> message1) {
+		for (int i = 0; i < message1.size(); i++) {
+			if (message1.get(i).contains("... --- ...")) {
+				return true;
+			}
+		}
+
 		return false;
 	}
 
-	public static double sortScores(List<Double> results) {
-		return 0.0;
+	public static List<Double> sortScores(List<Double> results) {
+		ArrayList<Double> clone = new ArrayList<Double>(results);
+		ArrayList<Double> sortedScores = new ArrayList<Double>();
+
+		double smallestValue;
+		john: for (int i = clone.size()-1; i >= 0; i--) {
+			smallestValue = 1000;
+
+			for (int k = 0; k < clone.size(); k++) {
+				if (clone.get(k) < smallestValue) {
+					smallestValue = clone.get(k);
+					
+				}
+			}
+			// smallestValue is now the smallest value in the clone array list
+
+			System.out.println(smallestValue);
+			// print smallest value
+			int smallestIndex = 0;
+			bill: for (int j = 0; j < clone.size(); j++) {
+				if (clone.get(j) == smallestValue) {
+					smallestIndex = j;
+
+					break bill;
+
+				}
+				clone.remove(smallestIndex);
+			}
+			// finds index of smallest value, then removes it
+			sortedScores.add(smallestValue);
+			// adds smallest value to the sorted Scores array list
+
+			
+			// if the size of sorted scores is equal to the size of the results array list,
+			// then break the john loop
+
+		}
+		
+		for(int i = 0; i < sortedScores.size(); i++) {
+			System.out.println(sortedScores.get(i));
+		}
+		return sortedScores;
 	}
 
 	public static String sortDNA(List<String> unsortedSequences) {
--- src/_03_More_Algorithms/_01_SearchingAndSortingAlgorithmsTest.java
+++ src/_03_More_Algorithms/_01_SearchingAndSortingAlgorithmsTest.java
@@ -68,15 +68,15 @@ public class _01_SearchingAndSortingAlgorithmsTest {
 
     // 6. Write a Morse code translator to read the messages. Just kidding!
 
-    // 7. Sort the exam results from lowest to highest.
-//    @Test
-//    public void testSort() throws Exception {
-//        List<Double> results = Arrays.asList(
-//                new Double[] { 60.3, 60.2, 80.4, 67.2, 95.0, 85.5, 40.7, 68.2, 38.4, 94.2, 85.0, 84.5, 50.1, 66.6 });
-//        assertEquals(38.4, Algorithms.sortScores(results).get(0), 0.0);
-//        assertEquals(40.7, Algorithms.sortScores(results).get(1), 0.0);
-//        assertEquals(95.0, Algorithms.sortScores(results).get(13), 0.0);
-//    }
+   //  7. Sort the exam results from lowest to highest.
+    @Test
+    public void testSort() throws Exception {
+      List<Double> results = Arrays.asList(
+              new Double[] { 60.3, 60.2, 80.4, 67.2, 95.0, 85.5, 40.7, 68.2, 38.4, 94.2, 85.0, 84.5, 50.1, 66.6 });
+      assertEquals(38.4, Algorithms.sortScores(results).get(0), 0.0);
+      assertEquals(40.7, Algorithms.sortScores(results).get(1), 0.0);
+      assertEquals(95.0, Algorithms.sortScores(results).get(13), 0.0);
+  }
 
      //8. Sort the DNA sequences by length from shortest to longest.
     @Test
